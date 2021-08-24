91a6f3f01df0c40b0b3541379abffcf872eadd1f
Az-s <azim.sa@yahoo.com>
2021-08-24 17:30:48 +0600

    some style fixes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
       <Router>
         <Navbar bg="dark" variant="dark" sticky='top'>
           <Container>
-            <Navbar.Brand href="/">Travel</Navbar.Brand>
+            <Navbar.Brand><NavLink to='/'>Travel</NavLink></Navbar.Brand>
             <Nav className="me-auto">
               <NavLink to='/' exact activeStyle={{ color: '#4e5559' }}>Home</NavLink>
               <NavLink to='/video' exact activeStyle={{ color: '#4e5559' }}>Video</NavLink>
@@ -29,7 +29,7 @@ const App = () => {
           </Container>
         </Navbar>
 
-        {/* <NavBar />  вариант через bootstrap не уверен что годится именно под это задание*/}
+        {/* <NavBar />  вариант через bootstrap не уверен что годится именно под это задание(каждая страница перезагружается)*/}
 
         <Route exact path='/' component={MainContent} />
         <Route path='/video' component={Video} />
